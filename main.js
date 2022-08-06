845a861778909b32d872d93542b258d7ff68ed6e
Okechukwu Collins Vizion <106563563+CollinsVizion35@users.noreply.github.com>
2022-08-06 20:41:08 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,82 @@
+const vanNot = document.getElementById('van');
+const van2Ya = document.getElementById('van2');
+const rainBow2 = document.getElementById('rainbow-image2');
+const man1 = document.getElementById('man1');
+const man2 = document.getElementById('man2');
+const man3 = document.getElementById('man3');
+const man4 = document.getElementById('man4');
+const man5 = document.getElementById('man5');
+const man6 = document.getElementById('man6');
+const man7 = document.getElementById('man7');
+const man8 = document.getElementById('man8');
+const man9 = document.getElementById('man9');
+const man10 = document.getElementById('man10');
+const lukaTitle = document.getElementsByClassName('luka-title');
+const vanLight = document.querySelector('.van-light')
+
+
+setTimeout (() => {
+    vanLight.style.visibility = 'hidden';
+}, 6000);
+
+
+setTimeout (() => {
+    vanNot.style.visibility = 'hidden';
+    van2Ya.style.visibility = 'visible';
+    rainBow2.style.visibility = 'visible';
+}, 8000);
+
+setTimeout (() => {
+    man1.style.visibility = 'visible';
+}, 9000);
+
+setTimeout (() => {
+    man1.style.visibility = 'hidden';
+    man2.style.visibility = 'visible';
+}, 10200);
+
+setTimeout (() => {
+    man2.style.visibility = 'hidden';
+    man3.style.visibility = 'visible';
+}, 10500);
+
+setTimeout (() => {
+    man3.style.visibility = 'hidden';
+    man4.style.visibility = 'visible';
+}, 10800);
+
+setTimeout (() => {
+    man4.style.visibility = 'hidden';
+    man5.style.visibility = 'visible';
+}, 11100);
+
+setTimeout (() => {
+    man5.style.visibility = 'hidden';
+    man6.style.visibility = 'visible';
+}, 11400);
+
+setTimeout (() => {
+    man6.style.visibility = 'hidden';
+    man7.style.visibility = 'visible';
+}, 11700);
+
+setTimeout (() => {
+    man7.style.visibility = 'hidden';
+    man8.style.visibility = 'visible';
+}, 12000);
+
+setTimeout (() => {
+    man8.style.visibility = 'hidden';
+    man9.style.visibility = 'visible';
+}, 12300);
+
+setTimeout (() => {
+    man9.style.visibility = 'hidden';
+    man10.style.visibility = 'visible';
+}, 12600);
+
+setTimeout (() => {
+    man10.style.visibility = 'hidden';
+    lukaTitle.style.visibility = 'visible';
+}, 12900);
+
